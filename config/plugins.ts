7d10fe4904553308b29d44c048cd1691427bac2e
Micah Alconcel <micah.alconcel@gmail.com>
2025-07-29 22:25:51 -0700

    placeholder jwt

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+// path: ./config/plugins.ts
+
+const crypto = require('crypto');
+
+export default ({ env }) => ({
+  // ...
+  'users-permissions': {
+    config: {
+      jwt: {
+        jwtSecret: env('STRAPI_JWT_SECRET') || crypto.randomBytes(16).toString('base64')
+      },
+    },
+  },
+});
